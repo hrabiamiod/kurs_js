b49da6421573c3fddfafe14cae87e7ebb87f7138
Michal W <majkel.design@gmail.com>
2017-08-30 22:48:06 +0000

    pierwszy commit

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function sprawdz() {
-    var liczba = document.getElementById("pole").value;
-    
-    if (liczba>0) document.getElementById("wynik").innerHTML="dodatnia";
-    else if (liczba<0) document.getElementById("wynik").innerHTML="ujenma";
-    else if (liczba==0) document.getElementById("wynik").innerHTML="zero";
-    else document.getElementById("wynik").innerHTML="to nie jest liczba";
-    
-}
